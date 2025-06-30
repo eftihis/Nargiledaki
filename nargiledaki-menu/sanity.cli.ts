053aa71d4d7550420a167a501d8f270e4a8c4aa4
Eftihis <eftihisluke@gmail.com>
2025-06-30 17:57:25 +0300

    Update dependencies and configuration: Upgrade @sanity/vision and sanity to version 3.95.0, add react-is, and set studioHost in sanity.cli.ts for improved project configuration.

--- nargiledaki-menu/sanity.cli.ts
+++ nargiledaki-menu/sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: 'hfmr6n6v',
     dataset: 'production'
   },
+  studioHost: 'nargiledaki-menu',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
